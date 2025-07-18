bf0040adac3399757a1cf85d839966cd1306ea88
esitorsunil <sunilrio23@gmail.com>
2025-07-18 18:33:00 +0530

    for loop1

--- warmup/loop/all even num in an array/app.js
+++ warmup/loop/all even num in an array/app.js
@@ -1,14 +1,70 @@
-var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Without loop (repetitive)
+console.log("Hello World");
+console.log("Hello World");
 
-var evenNum = function(arr) {
-    var even = [];
-
-    for (var i=0; i<arr.length; i++) {
-        if (arr[i]%2 == 0) {
-            even.push(arr[i]);
-        }
-    }
-    return even;
+// With loop (efficient)
+for (let i = 0; i < 2; i++) {
+  console.log("Hello World");
 }
 
-console.log(evenNum(a));
+//increment by 2 
+for (let i=0; i<10; i+=2){
+    console.log(i);
+}
+
+//function inside loop
+function greet(i) {
+    console.log("Hello" + i);
+}
+
+for (let i=0; i<5; i++){
+    greet(i);
+}
+
+
+// Loop inside array 
+let arr = [3, 7, 63, 86, 99]
+
+for (let i=0; i<arr.length; i++) {
+    console.log("Element at index "+ i + ": "+ arr[i]);
+}
+
+for (let i=0; i<arr.length; i++){
+    if(arr [i] %2 === 0) {
+        console.log("Even numbers "+ arr[i]);
+    }
+}
+
+// while loop
+
+let i=0;
+
+while (i<5){
+    console.log(i);
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
