42fbb8b48fd7703be3e7a0614407f990bc6eea89
esitorsunil <sunilrio23@gmail.com>
2025-07-25 21:30:15 +0530

    interverted triangle decreasing col (star, num)

--- warmup/star pattern/app.js
+++ warmup/star pattern/app.js
@@ -45,5 +45,30 @@ for (let i=0; i<n; i++){
 }
 
 
+// 05. Inverted triangle with decreasing columns
+
+//let n= 4
+for (let i=0; i<n; i++){
+    let row=""
+    for(let j=0; j<=n-i; j++){
+        row+= j+1 + " "
+    }
+    console.log(row)
+}
+
+// 06. Inverted triangle
+//let n = 5
+
+for(let i=0; i<n ; i++) {
+    let row = ""
+    for(let j=0; j< n-i; j++) {
+        row += "*"
+    }
+    console.log(row)
+}
+
+//
+
+
 
 
