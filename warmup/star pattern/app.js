9e3552d249fc25ec3c74833ecdb205fc1cb88f37
esitorsunil <sunilrio23@gmail.com>
2025-07-25 22:24:56 +0530

    binary pattern (alternative toggle & continous toggle across rows)

--- warmup/star pattern/app.js
+++ warmup/star pattern/app.js
@@ -67,8 +67,56 @@ for(let i=0; i<n ; i++) {
     console.log(row)
 }
 
-//
+//07. Right align star angle trinangle
 
+//let n = 5
 
+for (let i=0; i<n; i++) {
+    let row= ""
+    for (let j=0; j< n - (i + 1); j++){
+         row += " "
+    }
+    for (let k=0; k< i+1; k++) {
+        row += "*"
+    }
+    console.log(row)
+}
 
+//08. Binary pattern with alternating toggle
+
+//let n = 5
+
+for(let i=0; i<n; i++) {
+    let row = "";
+    let toggle = 1;
+    for (let j=0; j<i+1; j++) {
+        row = row + toggle
+        if(toggle == 1)   {
+            toggle = 0;
+        }else {
+            toggle = 1
+        }
+        
+    }
+    console.log(row)
+}
+
+// 09. Binary pattern with continuous toggle across rows
+
+//let n = 5
+let toggle = 1;
+for(let i=0; i<n; i++) {
+    let row = "";
+
+    for (let j=0; j<i+1; j++) {
+        row = row + toggle
+        if(toggle == 1)   {
+            toggle = 0;
+        }else {
+            toggle = 1
+        }
+        
+    }
+    console.log(row)
+}
 
