64d006b4ecfa4fe472a7502611efd1770a70b986
esitorsunil <sunilrio23@gmail.com>
2025-07-25 22:32:29 +0530

    center pyramid

--- warmup/star pattern/app.js
+++ warmup/star pattern/app.js
@@ -120,3 +120,17 @@ for(let i=0; i<n; i++) {
     console.log(row)
 }
 
+// center pyramid
+
+//let n = 4
+
+for (let i=0; i<n; i++) {
+    let row=""
+    for (let j=0; j< n- (i+1); j++) {
+        row += " "
+    }
+    for (let k=0; k< i * 2+1; k++) {
+        row += "*"
+    }
+    console.log(row)
+}
