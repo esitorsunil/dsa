311f5e746aaa162fd20dcc5f5e0be43be8372e92
esitorsunil <sunilrio23@gmail.com>
2025-09-08 21:37:07 +0530

    update

--- top 20 interview Ques/non repaeating character String/app.js
+++ top 20 interview Ques/non repaeating character String/app.js
@@ -1,14 +1,16 @@
 function nonRepeatingCharacter(str) {
     let frequency = {}
 
-    for (let char of str) {
+    for (let i=0; i< str.length; i++) {
+        let char = str [i]
         if(frequency[char]) {
             frequency[char]++
         }else {
             frequency[char] = 1
         }
 
-        for(let char of str) {
+        for(let i=0; i< str.length; i++) {
+            let char = str[i]
             if(frequency[char] === 1) {
                 return char
             }
