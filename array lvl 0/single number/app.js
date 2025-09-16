b1578e4fe13df56d084dc9227a3dac38d689b8ea
esitorsunil <sunilrio23@gmail.com>
2025-09-16 23:07:44 +0530

    single number best approach

--- array lvl 0/single number/app.js
+++ array lvl 0/single number/app.js
@@ -1,3 +1,6 @@
+
+// Approach 1: hashmap space Cpmplexity 0(n)
+
 var singleNumber = function(nums) {
   let hash = {}
 
@@ -13,4 +16,16 @@ var singleNumber = function(nums) {
           return nums[i]
       }
   }
+};
+
+
+// Approach2 : Bitwise XOR space complexity 0(1)
+
+var singleNumber = function(nums) {
+  let x = 0
+
+  for(let i=0; i<nums.length; i++) {
+    x = x ^ nums[i]
+  }
+  return x
 };
