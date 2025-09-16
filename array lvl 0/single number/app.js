20ef4c7bdf69cc1b6618e7b3d25bcaded301cc80
esitorsunil <sunilrio23@gmail.com>
2025-09-16 22:56:14 +0530

    single number

---
+++ array lvl 0/single number/app.js
@@ -0,0 +1,16 @@
+var singleNumber = function(nums) {
+  let hash = {}
+
+  for(let i=0; i<nums.length; i++) {
+      if(!hash[nums[i]]) {
+          hash[nums[i]] = 1
+      }else {
+          hash[nums[i]]++
+      }
+  }
+  for(let i=0; i<nums.length; i++) {
+      if(hash[nums[i]] == 1) {
+          return nums[i]
+      }
+  }
+};
