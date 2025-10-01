b1ca95ae4e1ea9509e5fdf1c2265b2958b53620c
esitorsunil <sunilrio23@gmail.com>
2025-10-01 20:19:12 +0530

    String: Anagram app2

--- strings/Anagrams/app.js
+++ strings/Anagrams/app.js
@@ -12,3 +12,30 @@ function anagrams(strs) {
     }
     return [...Object.values(map)]
 }
+
+
+//APPROACH 2
+
+var groupAnagrams = function(strs) {
+    let map = {}
+
+    for(let i=0; i<strs.length; i++) {
+        let freqArr = Array(26).fill(0)
+        let s = strs[i]
+
+        for(let j=0; j<s.length; j++) {
+            let index = s[j].charCodeAt()-"a".charCodeAt()
+            freqArr[index]++
+        }
+         let key = ""
+    for(let k=0; k<26; k++) {
+        key = key + "#" + freqArr[k];
+    }
+      if(!map[key]) {
+        map[key] = [s];
+    } else {
+        map[key].push(s)
+    }
+    }
+    return [...Object.values(map)]
+};
